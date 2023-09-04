380d1dd474d9f0a5f16c635bcbaaa3a2e35f32a2
Tophat218 <ST10356680@vgconnect.edu.za>
2023-09-04 11:11:33 +0200

    Added Kayla

---
+++ GitHub-Example/HogwartsStudentKayla.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+public class HogwartsStudentKayla : HogwartsStudent
+{
+    public override string Name { get { return "Kayla"; } }
+    public HogwartsStudentKayla() : base(100, 20, House.Slytherin) { }
+
+    protected override string SayCatchPhrase()
+    {
+        return "This Fucking Laptop!";
+    }
+    protected override string CastSpell()
+    {
+        return "Summon Familiar";
+    }
+}
