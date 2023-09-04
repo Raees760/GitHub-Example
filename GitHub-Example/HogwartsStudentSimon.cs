34a315507964121a991938182f07d2a439cf0d07
Tophat218 <ST10356680@vgconnect.edu.za>
2023-09-04 11:01:00 +0200

    Add changes to HogwartsStudent and adding my class

---
+++ GitHub-Example/HogwartsStudentSimon.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+public class HogwartsStudentSimon : HogwartsStudent
+{
+    public override string Name { get { return "Simon"; } }
+    public HogwartsStudentSimon() : base(100, 20, House.Slytherin) { }
+
+    protected override string SayCatchPhrase()
+    {
+        return "KILL HIM! KIIIILLL HIIIIMMM";
+    }
+    protected override string CastSpell()
+    {
+        return "I Cast Fist";
+    }
+}
